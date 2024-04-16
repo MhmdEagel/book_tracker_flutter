2ad8c663a38c02c5c31def29e1fa8898076b3e7f
Muhammad Eagel Triutama <muhammadeagel@gmail.com>
2024-04-16 09:38:24 +0700

    tutorial 7

--- lib/menu.dart
+++ lib/menu.dart
@@ -1,4 +1,6 @@
+import 'package:book_tracker/widgets/left_drawer.dart';
 import 'package:flutter/material.dart';
+import 'package:book_tracker/trackerlist_form.dart';
 
 class MyHomePage extends StatelessWidget {
   MyHomePage({super.key});
@@ -62,55 +64,61 @@ class MyHomePage extends StatelessWidget {
           ),
         ),
       ),
+      drawer: const LeftDrawer(),
     );
   }
 }
 
 class TrackerCard extends StatelessWidget {
-    final TrackerItem item;
+  final TrackerItem item;
 
-    const TrackerCard(this.item, {super.key}); // Constructor
+  const TrackerCard(this.item, {super.key}); // Constructor
 
-    @override
-    Widget build(BuildContext context) {
+  @override
+  Widget build(BuildContext context) {
     return Material(
-        color: Colors.indigo,
-        child: InkWell(
+      color: Colors.indigo,
+      child: InkWell(
         // Area responsive terhadap sentuhan
         onTap: () {
-            // Memunculkan SnackBar ketika diklik
-            ScaffoldMessenger.of(context)
-            ..hideCurrentSnackBar()
-            ..showSnackBar(SnackBar(
-                content: Text("Kamu telah menekan tombol ${item.name}!")));
+          // Memunculkan SnackBar ketika diklik
+          // ScaffoldMessenger.of(context)
+          //   ..hideCurrentSnackBar()
+          //   ..showSnackBar(SnackBar(
+          //       content: Text("Kamu telah menekan tombol ${item.name}!")));
+          if (item.name == "Tambah Buku") {
+            Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => const TrackerFormPage()));
+          }
         },
         child: Container(
-            // Container untuk menyimpan Icon dan Text
-            padding: const EdgeInsets.all(8),
-            child: Center(
+          // Container untuk menyimpan Icon dan Text
+          padding: const EdgeInsets.all(8),
+          child: Center(
             child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
                 Icon(
-                    item.icon,
-                    color: Colors.white,
-                    size: 30.0,
+                  item.icon,
+                  color: Colors.white,
+                  size: 30.0,
                 ),
                 const Padding(padding: EdgeInsets.all(3)),
                 Text(
-                    item.name,
-                    textAlign: TextAlign.center,
-                    style: const TextStyle(color: Colors.white),
+                  item.name,
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(color: Colors.white),
                 ),
-                ],
+              ],
             ),
-            ),
+          ),
         ),
-        ),
+      ),
     );
-    }
+  }
 }
-
 
 class TrackerItem {
   final String name;
